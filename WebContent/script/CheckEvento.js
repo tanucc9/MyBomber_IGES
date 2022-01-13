f83473fcc6109baea3f845bd3986f675e2fab8b8
vincenzo10b <vincimon00@outlook.com>
2022-01-13 12:28:47 +0100

    Modifica di CheckEvento.js

--- WebContent/script/CheckEvento.js
+++ WebContent/script/CheckEvento.js
@@ -10,15 +10,6 @@ function checkNome(inputtxt) {
 }
 
 
-function checkStruttura(inputtxt) {
-	var struttura = /^[A-Za-z\'_.-]*$/;
-	if(inputtxt.match(struttura))
-		return true
-
-	return false;	
-}
-
-
 /*function checkData(inputtxt) {
 	var data =  /^\d{1,2}\d{1,2}\d{4} \d{1,2}:\d{1,2}$/;
 	if(inputtxt.match(data)) 
@@ -32,23 +23,13 @@ function validate(obj) {
 	var valid = true;	
 	
 	var nome = document.getElementsByName("nome")[0].value;
-	if(!checkNome(nome)) {
+	if(!checkNome(nome) || !(nome.length>1) || !(nome.length<30)) {
 		valid = false;
 		let errNome = document.getElementById("errNome");
 		document.getElementById("errNome").innerHTML = "nome non valido" ;
 		errNome.style.color = "red";
 	} else {
 		document.getElementById("errNome").innerHTML = "" ;
-	}
-	
-	var struttura = document.getElementsByName("struttura")[0].value;
-	if(!checkStruttura(struttura)) {
-		valid = false;
-		let errStruttura = document.getElementsByName("struttura");
-		document.getElementById("errStruttura").innerHTML = "nome struttura non valido" ;
-		errStruttura.style.color = "red";
-	} else {
-		document.getElementById("errStruttura").innerHTML = "" ;
 	}
 	
 	if(valid)
