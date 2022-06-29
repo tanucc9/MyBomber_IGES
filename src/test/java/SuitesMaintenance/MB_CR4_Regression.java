4c56354d5f5b2030bb532c83c27d2f258dca370f
tanucc9 <maurogaetano997@gmail.com>
2022-06-29 15:31:31 +0200

    regression cr4

---
+++ src/test/java/SuitesMaintenance/MB_CR4_Regression.java
@@ -0,0 +1,26 @@
+package SuitesMaintenance;
+
+import integrazione.control.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+import unita.control.*;
+import unita.model.TestEventoDAO;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+        TestAreaUtenteServlet.class,
+        TestCreaEventoServlet.class,
+        TestEventiRecentiServlet.class,
+        TestPartecipaEventiServlet.class,
+        TestRichiesteEventiServlet.class,
+        TestEventoDAO.class,
+        TestRecensioneServlet.class,
+        TestIntegrazionePartecipaEventi.class,
+        TestIntegrazioneRecensione.class,
+        TestIntegrazioneCreaEvento.class,
+        TestIntegrazioneRichiesteEventi.class,
+        TestIntegrazioneEventiRecenti.class
+})
+
+public class MB_CR4_Regression {}
