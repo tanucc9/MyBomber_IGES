019172b366950ac85043dbea5e868189e9dec051
tanucc9 <maurogaetano997@gmail.com>
2022-07-01 13:57:38 +0200

    Store team servlet

--- src/main/java/control/squadra/CreaSquadraServlet.java
+++ src/main/java/control/squadra/CreaSquadraServlet.java
@@ -5,12 +5,20 @@ import java.sql.Date;
 import java.sql.SQLException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import log.LoggerSingleton;
 import model.evento.EventoBean;
 import model.evento.EventoDAO;
+import model.squadra.LogoSquadraBean;
+import model.squadra.LogoSquadraDAO;
+import model.squadra.SquadraBean;
+import model.squadra.SquadraDAO;
 import model.struttura.StrutturaBean;
 import model.struttura.StrutturaDAO;
 import model.utente.gestore.GestoreBean;
@@ -23,6 +31,7 @@ public class CreaSquadraServlet extends HttpServlet {
     /** The Constant serialVersionUID. */
     private static final long serialVersionUID = 1L;
 
+    private SquadraDAO squadraDao;
 
     /**
      * Do get.
@@ -36,7 +45,9 @@ public class CreaSquadraServlet extends HttpServlet {
      */
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {}
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
 
     /**
      * Do post.
@@ -51,7 +62,34 @@ public class CreaSquadraServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        doGet(request, response);
+        String nome = request.getParameter("nomeSquadra");
+        String nomeAbbr = request.getParameter("nomeAbbr");
+        String citta = request.getParameter("citta");
+        int idLogo = Integer.parseInt(request.getParameter("id_logo_scelto"));
+        String descr = request.getParameter("descr");
+
+        if (this.squadraDao == null) {
+            this.squadraDao = new SquadraDAO();
+        }
+
+        SquadraBean squadra = new SquadraBean();
+        squadra.setNome(nome);
+        squadra.setNomeAbbreviato(nomeAbbr);
+        squadra.setCitta(citta);
+        squadra.setDescrizione(descr);
+        squadra.setLogo(idLogo);
+        try {
+            this.squadraDao.doSave(squadra);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    public SquadraDAO getSquadraDao() {
+        return squadraDao;
+    }
+
+    public void setSquadraDao(SquadraDAO squadraDao) {
+        this.squadraDao = squadraDao;
+    }
 }
